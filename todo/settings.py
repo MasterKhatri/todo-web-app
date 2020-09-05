534848f29f7df7eb92f07c1f259179aa35cf7bc2
Akash Khatri <akash.khatri2000@gmail.com>
2020-09-06 02:28:52 +0530

    settings.py: set allowed hosts to none

--- todo/settings.py
+++ todo/settings.py
@@ -26,12 +26,7 @@ with open('/etc/secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    server {
-    listen 80 default_server;
-    return 444;
-}
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
